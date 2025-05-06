9d3bb3c686d8396eb83fa1049ba8340d05fff32b
CeceliaCD <cecelia.chollettedickson@rutgers.edu>
2025-05-05 23:52:53 -0400

    Removed some code from config loader and also changed botocore version

--- setup.py
+++ setup.py
@@ -5,10 +5,19 @@ setup(
     version='0.1.0',
     packages=find_packages(),
     install_requires=[
-        'pandas',
-        'requests',
-        'pyarrow',
-        'avro-python3'
+        "numpy<1.25.0,>=1.21.0",
+        "pandas<2.0.0,>=1.2.0",
+        "pyarrow<7.1.0,>=2.0.0",
+        "charset-normalizer<3.0,>=2.0",
+        "requests<2.27.2,>=2.23.0",
+        "urllib3<1.27,>=1.25.4",
+        "pytz<2022.2,>=2020.1",
+        "botocore==1.24.21",
+        "awswrangler==2.15.1",
+        "scipy==1.8.0",
+        "redshift-connector==2.0.907",
+        "charset-normalizer<3.0,>=2.0",
+        "avro-python3"
     ],
     include_package_data=True,
     description='The .whl package of pokemon datalake and analytics project.',
--- utils/config_loader.py
+++ utils/config_loader.py
@@ -2,10 +2,6 @@ import json
 import os
 
 def load_json_config(path):
-    if not os.path.isabs(path):
-        possible_path = os.path.join('/tmp', path)
-        if os.path.exists(possible_path):
-            path = possible_path
     with open(path, "r") as f:
         config_file = json.load(f)
         return config_file
